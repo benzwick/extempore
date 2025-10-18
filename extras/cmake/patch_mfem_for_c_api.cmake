ad4f237dd07a2126facf132ffac977f0747002a7
Ben Zwick <benzwick@gmail.com>
2025-10-18 16:39:15 +0800

    Integrate MFEM C API wrapper into build system
    - Created CMake patch script to inject C API sources into MFEM build
    - Modified MFEM ExternalProject to run patch during build
    - Patch copies mfem_c_api.{h,cpp} into MFEM source directory
    - Modifies MFEM's CMakeLists.txt to compile C API with library
    - C API functions will be available in libmfem.so for XTLang binding
    
    This allows Extempore's XTLang to call MFEM through C functions
    without needing a separate wrapper library or Rust/SWIG tooling.

---
+++ extras/cmake/patch_mfem_for_c_api.cmake
@@ -0,0 +1,33 @@
+# CMake script to patch MFEM's CMakeLists.txt to include C API wrapper
+# This is run during the PATCH_COMMAND step of ExternalProject_Add
+
+# Copy C API files
+file(COPY
+  "${MFEM_CAPI_SOURCE_DIR}/mfem_c_api.h"
+  "${MFEM_CAPI_SOURCE_DIR}/mfem_c_api.cpp"
+  DESTINATION "${MFEM_SOURCE_DIR}")
+
+# Read MFEM's CMakeLists.txt
+file(READ "${MFEM_SOURCE_DIR}/CMakeLists.txt" MFEM_CMAKE_CONTENT)
+
+# Check if already patched (to make this idempotent)
+string(FIND "${MFEM_CMAKE_CONTENT}" "mfem_c_api.cpp" ALREADY_PATCHED)
+
+if(ALREADY_PATCHED EQUAL -1)
+  # Find the line that adds mfem library sources
+  # MFEM's CMakeLists.txt has: add_library(mfem ${SOURCES} ${HEADERS})
+  # We want to add our C API source to SOURCES before that line
+
+  string(REPLACE
+    "add_library(mfem \${SOURCES} \${HEADERS})"
+    "list(APPEND SOURCES mfem_c_api.cpp)\nlist(APPEND HEADERS mfem_c_api.h)\nadd_library(mfem \${SOURCES} \${HEADERS})"
+    MFEM_CMAKE_CONTENT
+    "${MFEM_CMAKE_CONTENT}")
+
+  # Write the patched CMakeLists.txt back
+  file(WRITE "${MFEM_SOURCE_DIR}/CMakeLists.txt" "${MFEM_CMAKE_CONTENT}")
+
+  message(STATUS "Patched MFEM CMakeLists.txt to include C API wrapper")
+else()
+  message(STATUS "MFEM CMakeLists.txt already patched for C API")
+endif()
